5159a716710d44de5154272c429b26048db6eeba
Robert Salaiz <work.salaizrm@gmail.com>
2020-04-22 03:15:54 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -47,7 +47,7 @@ class ApplicationController < Sinatra::Base
   end
 
 
-  delete '/posts/:id/delete' do
+  delete '/articles/:id/delete' do
     @articles = Article.find_by_id(params[:id])
     @articles.delete
     erb :deleted
